bc59cca5e04ad3fcd18445b529af353afbb37e69
Bradley Stannard <bstannard@petsathome.co.uk>
2023-06-21 13:40:33 +0100

    Added action.yml file

---
+++ install.sh
@@ -0,0 +1,4 @@
+mkdir bin
+wget -O bin/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/$TERRAGRUNT_VERSION/terragrunt_linux_amd64
+chmod +x bin/terragrunt
+echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
