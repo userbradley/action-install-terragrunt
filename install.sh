72e4b0484fb9beced732768cded1b26d4ef8fed1
Bradley Stannard <bstannard@petsathome.co.uk>
2023-06-21 14:18:53 +0100

    Finishing touches

--- install.sh
+++ install.sh
@@ -2,5 +2,3 @@ mkdir bin
 wget -O bin/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/$VERSION/terragrunt_linux_amd64
 chmod +x bin/terragrunt
 echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
-
-#echo $terragrunt-version
