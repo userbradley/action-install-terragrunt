a1e7657b97684d30c0e45c0b40ba7dee43ede204
Bradley Stannard <bstannard@petsathome.co.uk>
2023-06-21 14:00:20 +0100

    Testing workflow

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#mkdir bin
-#wget -O bin/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/$TERRAGRUNT_VERSION/terragrunt_linux_amd64
-#chmod +x bin/terragrunt
-#echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
+mkdir bin
+wget -O bin/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/$$terragrunt-version/terragrunt_linux_amd64
+chmod +x bin/terragrunt
+echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
 
-echo $terragrunt-version
+#echo $terragrunt-version
